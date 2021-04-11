8062eb47a47af264be301ea839905d69cedd6f81
Konrad Jówko <konrad.jowko@gmail.com>
2021-04-11 21:51:46 +0200

    Add script for removing active Article class

--- js/script.js
+++ js/script.js
@@ -12,7 +12,19 @@
 
     /* remove class 'active' from all article links  */
 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
     /* add class 'active' to the clicked link */
+
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
     /* remove class 'active' from all articles */
 
